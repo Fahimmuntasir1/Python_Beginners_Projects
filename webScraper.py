4f0f347a2d9029571f63d819d3e6bb7214fd525b
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-09 10:35:43 -0700

    web scraping tool is ready to use

--- webScraper.py
+++ webScraper.py
@@ -1,1 +1,13 @@
 # Build a simple web scraper with python.
+
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("https://www.simpleweb.org/")
+
+
+soup = BeautifulSoup(res.content, "html.parser")
+search = soup.find("table", class_="mainContent")
+content = search.find_all("p")
+
+print(content)
